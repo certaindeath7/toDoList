fda36438a06bb3c0064c49e10a3cd48342285219
Duc Nguyen <ducnguyentranphuc98@gmail.com>
2020-05-04 03:32:02 +1000

    get hold of the data and save it into localstorage

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -5,20 +5,104 @@ var app = {
 
         // DeviceReady method
         document.addEventListener('deviceready', this.loadToDoList.bind(this), false);
-
-        //when user moves to other app
-        document.addEventListener('pause', this.loadToDoList.bind(this), false);
-
-        // when user reopens the app method
-        document.addEventListener('resume', this.loadToDoList.bind(this), false);
-
     },
 
 
+    // device ready method
     loadToDoList: function () {
 
+        //getting elements from html file
+        const taskList = document.getElementById("taskList");
+        const taskInput = document.getElementById("input");
+        const deleteTask = document.getElementById("doneBtn");
+        const addTask = document.getElementById("addBtn");
+
+        // assign icons classes as variables
+        const DONE = "glyphicon glyphicon-ok";
+        const UNDONE = "glyphicon glyphicon-remove";
+        const STRIKE_THROUGH = "lineThrough";
+
+        // create variables for Key-Value localstorage
+        let USER_TASK, taskId;
+
+        // check if Key-Value table has items
+        if (window.localStorage.getItem("USER_TASK")) {
+
+            // convert text into javascript object
+            USER_TASK = JSON.parse(window.localStorage.getItem("USER_TASK"));
+
+            // set the id for each task
+            id = USER_TASK.length;
+
+            // present the list of tasks
+            loadTasks(USER_TASK);
+        } else {
+            USER_TASK =[];
+            taskId = 0;
+        }
+
+
+        // present the list of tasks
+        function loadTasks(array) {
+            //for each element in this array, add it to the task list
+            array.forEach(function(tasks){
+                addTasks(tasks.label, tasks.id, tasks.completed, tasks.remove);
+            });
+        }
+
+        //add tasks to the list
+        function addTasks(label, id, completed, remove) {
+
+            if(remove){ return; }
+
+            const COMPLETED = completed ? DONE : UNDONE;
+            const STRIKE = completed ? STRIKE : "";
+
+            // if a task is completed, pass in the icon as in javascript object
+
+            const tasks = `<li class="tasks">
+                                    <i class= "${COMPLETED}" job="complete" id="${id}"></i>
+                                    <p class="text ${STRIKE}">${label}</p>
+                                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                              </li>`;
+            
+            // insert tasks into "taskList" position
+            taskList.insertAdjacentHTML("beforeend", tasks);
+        }
+
+        // remove the all tasks from the list
+        deleteTask.addEventListener("click", function(){
+
+            window.localStorage.clear();
+
+        });
+
+        // add eventlistner for inputting a new task by clicking "return" or "enter key"
+        addTask.addEventListener("click", function(){
+                
+
+                //check if the list is empty
+                if(taskInput.value){
+                    addTasks(taskInput.value, taskId, false, false);
+
+                    //JSON format
+                    USER_TASK.push({
+                        label : taskInput.value,
+                        id: taskId,
+                        completed: false,
+                        remove: false
+                    });
+
+                    // insert data into the local storage. 
+                    window.localStorage.setItem("USER_TASK", JSON.stringify(USER_TASK)); //convert a javascript value to JSON
+
+                    taskId++;
+                }
+                taskInput.value = "";
+        });
 
     },
+
 
 
 };
--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -5,20 +5,103 @@ var app = {
 
         // DeviceReady method
         document.addEventListener('deviceready', this.loadToDoList.bind(this), false);
-
-        //when user moves to other app
-        document.addEventListener('pause', this.loadToDoList.bind(this), false);
-
-        // when user reopens the app method
-        document.addEventListener('resume', this.loadToDoList.bind(this), false);
-
     },
 
 
+    // device ready method
     loadToDoList: function () {
 
+        //getting elements from html file
+        const taskList = document.getElementById("taskList");
+        const taskInput = document.getElementById("taskInput");
+        const deleteTask = document.getElementById("doneBtn");
+    
+
+        // assign icons classes as variables
+        const DONE = "glyphicon glyphicon-ok";
+        const UNDONE = "glyphicon glyphicon-remove";
+        const STRIKE_THROUGH = "lineThrough";
+
+        // create variables for Key-Value localstorage
+        let USERS_TASK, taskId;
+
+        // check if Key-Value table has items
+        if (window.localStorage.getItem("USER_TASK")) {
+
+            // convert text into javascript object
+            USERS_TASK = JSON.parse(window.localStorage.getItem("USER_TASK"));
+
+            // set the id for each task
+            id = USERS_TASK.length;
+
+            // present the list of tasks
+            loadTasks(USERS_TASK);
+        }
+        // present the list of tasks
+        function loadTasks(array) {
+            //for each element in this array, add it to the task list
+            array.forEach(addTasks(label, id, completed, remove));
+        }
+
+        //add tasks to the list
+        function addTasks(label, id, completed, remove) {
+
+            const COMPLETED = completed;
+            const STRIKE = completed;
+
+            // if a task is completed, pass in the icon as in javascript object
+            if (completed) { COMPLETED = DONE }
+            else { UNDONE }
+
+            // if a task is completed, strike through the text
+            if (completed) { STRIKE = STRIKE_THROUGH }
+            else { "" }
+
+            const tasks = `<li class="taskList">
+                                    <i class= "${COMPLETED}" job="complete" id="${id}"></i>
+                                    <p class="text ${STRIKE}">${toDo}</p>
+                                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                              </li>`;
+            
+            // insert tasks into "taskList" position
+            taskList.insertAdjacentHTML("beforeend", tasks);
+        }
+
+        // remove the all tasks from the list
+        deleteTask.addEventListener("click", function(){
+
+            window.localStorage.clear();
+            window.localStorage.remove();
+        });
+
+        // add eventlistner for inputting a new task by clicking "return" or "enter key"
+        document.addEventListener("keyup", function(){
+                if(event.keycode == 13){
+                const toDoTask = taskInput.value;
+
+                //check if the list is empty
+                if(toDoTask){
+                    addTasks(toDoTask, taskId, null, null);
+
+                    //JSON format
+                    USERS_TASK.push({
+                        label : toDoTask,
+                        id: taskId,
+                        completed: null,
+                        remove: null
+                    });
+
+                    // insert data into the local storage. 
+                    window.localStorage.setItem("USER_TASK", JSON.stringify(USERS_TASK)); //convert a javascript value to JSON
+
+                    taskId++;
+                }
+                askInput.value = "";
+                }
+        });
 
     },
+
 
 
 };
